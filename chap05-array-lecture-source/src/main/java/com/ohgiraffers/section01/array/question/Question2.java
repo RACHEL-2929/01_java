2624c162f496356336af9eae3d68a456cc59e4c5
RACHEL-2929 <sts06154@naver.com>
2024-05-13 17:33:10 +0900

    looping

--- chap05-array-lecture-source/src/main/java/com/ohgiraffers/section01/array/question/Question2.java
+++ chap05-array-lecture-source/src/main/java/com/ohgiraffers/section01/array/question/Question2.java
@@ -1,2 +1,39 @@
-package main.java.com.ohgiraffers.section01.array.question;public class Question2 {
-}
+package main.java.com.ohgiraffers.section01.array.question;
+
+import java.util.Scanner;
+
+public class Question2 {
+    //2번째로 큰 값
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("배열의 길이를 설정해주세요 : ");
+        int number = sc.nextInt();
+        int[] array = new int[number];
+        int max = -10000000;
+
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("값을 입력해주세요 : ");
+            array[i] = sc.nextInt();
+        }
+        int secondMax = array[1];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                secondMax = max;
+                max = array[i];
+
+            } else if (array[i] < max && array[i] > secondMax) {
+                    System.out.println("!!");
+                    secondMax = array[i];
+            }
+
+
+        }
+        System.out.println("최댓값은 " + max);
+        System.out.println("두 번째로 큰 값은 " + secondMax);
+        }
+
+
+
+    }
+
