8145c890f6f51ccd01f5322cba1ed63af02e88e1
RACHEL-2929 <sts06154@naver.com>
2024-05-17 12:06:53 +0900

    modify

--- chap06-class-and-object-lecture/src/main/java/com/ohgiraffers/section05/parameter/RectAngle.java
+++ chap06-class-and-object-lecture/src/main/java/com/ohgiraffers/section05/parameter/RectAngle.java
@@ -39,7 +39,8 @@ public class RectAngle {
         System.out.println("사각형의 둘레는 " + area + "입니다.");
     }
 
-    //???
+    // Object 클래스에 있는 toString 을 활용한 것으로 원래는 주소값이 나오는 것을 재정의해서 내가 원하는 문자열을 돌려받기
+    // main 에서 사용할 때는 인스턴스 이름만 적으면 자동으로 .toString 이 적혀져서 아래 구문이 실행됨
     @Override  // 부모가 가진 기능을 자손이 재정의 해서 사용할 수 있다.
     public String toString() {
         return "RectAngle{" +
