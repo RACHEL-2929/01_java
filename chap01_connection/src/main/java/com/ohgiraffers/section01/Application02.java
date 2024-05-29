8e3bdedc6557d240e53e37308d8954921b8fdebf
RACHEL-2929 <sts06154@naver.com>
2024-05-29 13:34:14 +0900

    jdbc

---
+++ chap01_connection/src/main/java/com/ohgiraffers/section01/Application02.java
@@ -0,0 +1,30 @@
+package com.ohgiraffers.section01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Application02 {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/employee";
+        String user = "songpa";
+        String password = "songpa";
+
+        Connection con = null;
+
+        try {
+            con = DriverManager.getConnection(url,user,password);
+            System.out.println(con);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
+    }
+}
